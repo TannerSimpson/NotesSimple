0397eb2b17d40adf21c5306d6bec9dc38c35efd1
Tanner Simpson <tfs2@domain.com>
2024-05-13 14:13:58 -0400

    linking all of the pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./public/profile.html"],
+  content: ["./public/index.html", "./public/profile.html", "./public/main.html", "./public/signup.html", "./public/login.html"],
   theme: {
     extend: {},
   },
