9eda64c750b3f33ea5f13599213e1c0e8eaa6570
Jason-NotesSimple <133404379+Jason-NotesSimple@users.noreply.github.com>
2024-05-22 13:47:18 -0400

    Update signup.js
    updated the insert statement

--- public/signup.js
+++ public/signup.js
@@ -1,14 +1,11 @@
 // Get form data
-const getFormData = () => {
-  const uname = document.getElementById('username').value;
-  const pword = document.getElementById('password').value;
-  const fName = document.getElementById('firstname').value;
-  const lName = document.getElementById('lastname').value;
-  const emailaddress = document.getElementById('email').value;
-  const phonenumber = document.getElementById('phone').value;
-
-  return { uname, pword, fName, lName, emailaddress, phonenumber };
-};
+const uname = document.getElementById('username').value;
+const pword = document.getElementById('password').value;
+const fName = document.getElementById('firstname').value;
+const lName = document.getElementById('lastname').value;
+const emailaddress = document.getElementById('email').value;
+const phonenumber = document.getElementById('phone').value;
+const zcode = document.getElementById('zipcode').value;
 
 // Create connection pool to the database
 const { Pool } = require('pg');
@@ -22,29 +19,24 @@ const pool = new Pool({
 });
 
 (async () => {
-
   try {
     // Connect to the database
     const client = await pool.connect();
-
-    // Execute a query
-    const res = await client.query('SELECT * FROM Accounts');
     
-    const rows = res.rows;
-
-    console.log('Query results:', rows);
-    console.log('did this work');
+    // Create the query string
+    const qstring = "INSERT INTO accounts (username, password, firstname, lastname, email, phone, zipcode) VALUES ('$uname','$pword', '$fName', '$lName', '$emailaddress', $phonenumber, $zcode)";
+    
+    // Execute a query
+    const res = await client.query('$qstring');
 
     // Release the client connection back to the pool
     client.release();
 
   } catch (error) {
     console.error('Error connecting to database:', error);
-    console.log('did this work');
   } finally {
     // Close the connection pool when finished (optional)
     await pool.end();
   }
-
 });
 
