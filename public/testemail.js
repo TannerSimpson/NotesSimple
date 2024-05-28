a946cab32874169cd1aba7b03760bd65a6ea3c56
Jason-NotesSimple <133404379+Jason-NotesSimple@users.noreply.github.com>
2024-05-28 14:13:01 -0400

    Update testemail.js
    updated code

--- public/testemail.js
+++ public/testemail.js
@@ -1,6 +1,6 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-javascript
+
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
