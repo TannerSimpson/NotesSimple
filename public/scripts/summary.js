e92d2cb0a774baa475a61d36940858ff13710ae2
TannerSimpson <111939959+TannerSimpson@users.noreply.github.com>
2024-06-17 14:41:20 -0400

    Update summary.js manually

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,8 +1,8 @@
-import { pipeline } from "@xenova/transformers";
+/*import { pipeline } from "@xenova/transformers";
 
 const pipe = await pipeline("summarization");
 
 export async function getAndDisplaySummary(inputText) {
     const result = await pipe(inputText, { max_length: 100, min_length: 50 });
     return result[0].summary_text;
-}
+}*/
